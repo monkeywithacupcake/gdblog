d5c6e0ffba9e5b3fb4e773fa73fb3b7fdc41bc9c
Jess <jessachandler@gmail.com>
2018-05-03 14:47:05 +0900

    changed theme

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,26 @@
+var store = [{
+        "title": "Backstory",
+        "excerpt":"Every business has a backstory, ours just starts with guilt and FaceTime Picture This Two sisters live miles and miles apart, but chat frequently. Nearly every week, we spend time staring at each other and catching up on all of the exciting things (and real things) going on in our...","categories": ["inside"],
+        "tags": ["story"],
+        "url": "http://blog.gifts-done.com/backstory/",
+        "teaser":null},{
+        "title": "How to deliver a gift!",
+        "excerpt":"Suppliers are the best! We count on your creativity and professionalism to make better gifting for everyone. Your Dashboard When you are logged in as a gifts-done supplier, you can see all of the gift events assigned to you and their status. Most of the time, you will get just...","categories": ["suppliers"],
+        "tags": ["how-to"],
+        "url": "http://blog.gifts-done.com/how-to-supply-gifts-done/",
+        "teaser":"http://blog.gifts-done.com/assets/images/abstract-12.jpg"},{
+        "title": "How to use the gifts-done service",
+        "excerpt":"Using a new service can be confusing. We want everyone to feel confident when first starting out with gifts-done. Registration The gifts-done service is ready for you, with just your email and a password. You can get started in less than 10 seconds. Please pay attention to the email that...","categories": ["users"],
+        "tags": ["how-to"],
+        "url": "http://blog.gifts-done.com/how-to-use-gifts-done/",
+        "teaser":"http://blog.gifts-done.com/assets/images/abstract-12.jpg"},{
+        "title": "5 No Cost Ways to Spread Joy",
+        "excerpt":"“I’ve learned that people will forget what you said, people will forget what you did, but people will never forget how you made them feel.”― Maya Angelou We think that a lot is communicated in how people feel, and we want people to feel joy. That’s not just a gimmick....","categories": ["tips"],
+        "tags": ["gifts"],
+        "url": "http://blog.gifts-done.com/5ways-nocost/",
+        "teaser":"http://blog.gifts-done.com/assets/images/never-feel.png"},{
+        "title": "May Instagram Challenge",
+        "excerpt":"“You have brains in your head. You have feet in your shoes. You can steer yourself, any direction you choose.”― Dr. Seuss Our heart and soul is in our wonderful suppliers, and we want everyone to have a little fun. To that end, we created an instagram challenge for suppliers,...","categories": ["suppliers"],
+        "tags": ["social","instagram"],
+        "url": "http://blog.gifts-done.com/may-ig/",
+        "teaser":null}]
