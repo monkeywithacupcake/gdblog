d0e8223ee5797b1e37ebd57e3e170f60b57067e0
Jess <jessachandler@gmail.com>
2018-05-04 08:05:09 +0900

    add newsletter sign up and clean up

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -8,19 +8,29 @@ var store = [{
         "excerpt":"Suppliers are the best! We count on your creativity and professionalism to make better gifting for everyone. Your Dashboard When you are logged in as a gifts-done supplier, you can see all of the gift events assigned to you and their status. Most of the time, you will get just...","categories": ["suppliers"],
         "tags": ["how-to"],
         "url": "http://blog.gifts-done.com/how-to-supply-gifts-done/",
-        "teaser":"http://blog.gifts-done.com/assets/images/abstract-12.jpg"},{
+        "teaser":null},{
         "title": "How to use the gifts-done service",
         "excerpt":"Using a new service can be confusing. We want everyone to feel confident when first starting out with gifts-done. Registration The gifts-done service is ready for you, with just your email and a password. You can get started in less than 10 seconds. Please pay attention to the email that...","categories": ["users"],
         "tags": ["how-to"],
         "url": "http://blog.gifts-done.com/how-to-use-gifts-done/",
-        "teaser":"http://blog.gifts-done.com/assets/images/abstract-12.jpg"},{
+        "teaser":null},{
         "title": "5 No Cost Ways to Spread Joy",
         "excerpt":"“I’ve learned that people will forget what you said, people will forget what you did, but people will never forget how you made them feel.”― Maya Angelou We think that a lot is communicated in how people feel, and we want people to feel joy. That’s not just a gimmick....","categories": ["tips"],
         "tags": ["gifts"],
         "url": "http://blog.gifts-done.com/5ways-nocost/",
-        "teaser":"http://blog.gifts-done.com/assets/images/never-feel.png"},{
+        "teaser":null},{
+        "title": "Keep a Gift Log",
+        "excerpt":"Logging all of the things - it really helps. When I was in high school, I found a “Little Irish Birthday Book” on the clearance rack of the bookstore. The book was simply a list of every day that occurs in a year, so you can write a name. With...","categories": ["tips"],
+        "tags": ["gifts"],
+        "url": "http://blog.gifts-done.com/keep-gift-log/",
+        "teaser":null},{
         "title": "May Instagram Challenge",
         "excerpt":"“You have brains in your head. You have feet in your shoes. You can steer yourself, any direction you choose.”― Dr. Seuss Our heart and soul is in our wonderful suppliers, and we want everyone to have a little fun. To that end, we created an instagram challenge for suppliers,...","categories": ["suppliers"],
         "tags": ["social","instagram"],
         "url": "http://blog.gifts-done.com/may-ig/",
+        "teaser":null},{
+        "title": "3 Rules to Regift with Confidence",
+        "excerpt":"Worried about regifting? Never fear, there are rules for this. “My friend gave me this beautiful vase, but it has been in my cupboard for a year, and I know another friend would actual use it. Is it okay to regift.” “I just received two different desk sets when I...","categories": ["users"],
+        "tags": ["questions","gifts"],
+        "url": "http://blog.gifts-done.com/regift/",
         "teaser":null}]
