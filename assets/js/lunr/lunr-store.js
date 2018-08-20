da2b16f447c6877fdb008f552fc197f3d4e28a18
Jess <jessachandler@gmail.com>
2018-08-20 12:58:42 -0700

    updated view and added posts

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -3,64 +3,99 @@ var store = [{
         "excerpt":"Every business has a backstory, ours just starts with guilt and FaceTime Picture This Two sisters live miles and miles apart, but chat frequently. Nearly every week, we spend time staring at each other and catching up on all of the exciting things (and real things) going on in our...","categories": ["inside"],
         "tags": ["story"],
         "url": "http://blog.gifts-done.com/backstory/",
-        "teaser":null},{
+        "teaser":"http://blog.gifts-done.com/assets/images/abstract-7.jpg"},{
+        "title": "Setting up Preferences",
+        "excerpt":"What kind of information should I include in preferences? Gifts Done gives you several ways to include information to help our algorithm (and makers) deliver great gifts to your giftees. Your job is to break down what you know into words that we can understand. It sounds simple enough, but...","categories": ["inside","questions"],
+        "tags": ["gifts"],
+        "url": "http://blog.gifts-done.com/how-to-get-the-best-match/",
+        "teaser":"http://blog.gifts-done.com/assets/images/best-gift-match.png"},{
         "title": "How to deliver a gift!",
-        "excerpt":"Suppliers are the best! We count on your creativity and professionalism to make better gifting for everyone. Your Dashboard When you are logged in as a gifts-done supplier, you can see all of the gift events assigned to you and their status. Most of the time, you will get just...","categories": ["suppliers"],
-        "tags": ["how-to"],
+        "excerpt":"Suppliers are the best! We count on your creativity and professionalism to make better gifting for everyone. Your Dashboard When you are logged in as a gifts-done supplier, you can see all of the gift events assigned to you and their status. Most of the time, you will get just...","categories": ["inside"],
+        "tags": ["how-to","suppliers"],
         "url": "http://blog.gifts-done.com/how-to-supply-gifts-done/",
-        "teaser":null},{
+        "teaser":"http://blog.gifts-done.com/assets/images/abstract-12.jpg"},{
         "title": "How to use the gifts-done service",
-        "excerpt":"Using a new service can be confusing. We want everyone to feel confident when first starting out with gifts-done. Registration The gifts-done service is ready for you, with just your email and a password. You can get started in less than 10 seconds. Please pay attention to the email that...","categories": ["users"],
-        "tags": ["how-to"],
+        "excerpt":"Using a new service can be confusing. We want everyone to feel confident when first starting out with gifts-done. Registration The gifts-done service is ready for you, with just your email and a password. You can get started in less than 10 seconds. Please pay attention to the email that...","categories": ["inside"],
+        "tags": ["how-to","users"],
         "url": "http://blog.gifts-done.com/how-to-use-gifts-done/",
-        "teaser":null},{
+        "teaser":"http://blog.gifts-done.com/assets/images/abstract-12.jpg"},{
+        "title": "5 Fun Spring Gifts",
+        "excerpt":"Everyone is waking up and getting new energy - it is time for spring gifting! Give them to yourself or someone else for whatever occasion inspires you this spring. Our short spring gift idea list You don’t even have to buy all of this cool stuff. We don’t link to...","categories": ["tips"],
+        "tags": ["gifts"],
+        "url": "http://blog.gifts-done.com/fun-spring-gifts/",
+        "teaser":"http://blog.gifts-done.com/assets/images/spring-gift-ideas.png"},{
         "title": "5 No Cost Ways to Spread Joy",
         "excerpt":"“I’ve learned that people will forget what you said, people will forget what you did, but people will never forget how you made them feel.”― Maya Angelou We think that a lot is communicated in how people feel, and we want people to feel joy. That’s not just a gimmick....","categories": ["tips"],
         "tags": ["gifts"],
         "url": "http://blog.gifts-done.com/5ways-nocost/",
-        "teaser":null},{
+        "teaser":"http://blog.gifts-done.com/assets/images/abstract-12.jpg"},{
+        "title": "20 Canned Birthday Greetings",
+        "excerpt":"What do I say? I can be so hard to come up with something to say in a birthday greeting. You want to be witty but not crass, and kind but not weird. Here are several greetings that you can take as is or use to get your own writing...","categories": ["tips","questions"],
+        "tags": ["gifts"],
+        "url": "http://blog.gifts-done.com/20-canned-birthday/",
+        "teaser":"http://blog.gifts-done.com/assets/images/note.png"},{
         "title": "Keep a Gift Log",
         "excerpt":"Logging all of the things - it really helps. When I was in high school, I found a “Little Irish Birthday Book” on the clearance rack of the bookstore. The book was simply a list of every day that occurs in a year, so you can write a name. With...","categories": ["tips"],
         "tags": ["gifts"],
         "url": "http://blog.gifts-done.com/keep-gift-log/",
-        "teaser":null},{
+        "teaser":"http://blog.gifts-done.com/assets/images/abstract-12.jpg"},{
         "title": "May Instagram Challenge",
-        "excerpt":"“You have brains in your head. You have feet in your shoes. You can steer yourself, any direction you choose.”― Dr. Seuss Our heart and soul is in our wonderful suppliers, and we want everyone to have a little fun. To that end, we created an instagram challenge for suppliers,...","categories": ["suppliers"],
-        "tags": ["social","instagram"],
+        "excerpt":"“You have brains in your head. You have feet in your shoes. You can steer yourself, any direction you choose.”― Dr. Seuss Our heart and soul is in our wonderful suppliers, and we want everyone to have a little fun. To that end, we created an instagram challenge for suppliers,...","categories": ["inside"],
+        "tags": ["social","suppliers"],
         "url": "http://blog.gifts-done.com/may-ig/",
-        "teaser":null},{
+        "teaser":"http://blog.gifts-done.com/assets/images/abstract-12.jpg"},{
         "title": "3 Rules to Regift with Confidence",
-        "excerpt":"Worried about regifting? Never fear, there are rules for this. “My friend gave me this beautiful vase, but it has been in my cupboard for a year, and I know another friend would actual use it. Is it okay to regift.” “I just received two different desk sets when I...","categories": ["users"],
-        "tags": ["questions","gifts"],
+        "excerpt":"Worried about regifting? Never fear, there are rules for this. “My friend gave me this beautiful vase, but it has been in my cupboard for a year, and I know another friend would actual use it. Is it okay to regift.” “I just received two different desk sets when I...","categories": ["questions"],
+        "tags": ["gifts"],
         "url": "http://blog.gifts-done.com/regift/",
-        "teaser":null},{
+        "teaser":"http://blog.gifts-done.com/assets/images/abstract-12.jpg"},{
         "title": "How to Apologize for a Missed Gift",
-        "excerpt":"Many of our customers at Gifts Done are just like us (shocker) and have missed gifts in the past. In fact, most of the feedback that we get from users is how they love not doing that anymore because we take care of them. If you are not using an...","categories": ["users"],
-        "tags": ["questions","gifts"],
+        "excerpt":"Many of our customers at Gifts Done are just like us (shocker) and have missed gifts in the past. In fact, most of the feedback that we get from users is how they love not doing that anymore because we take care of them. If you are not using an...","categories": ["questions"],
+        "tags": ["gifts"],
         "url": "http://blog.gifts-done.com/apologize/",
-        "teaser":null},{
+        "teaser":"http://blog.gifts-done.com/assets/images/abstract-7.jpg"},{
+        "title": "Expanding Birthday Greetings",
+        "excerpt":"What do I say? Are you a little unsure of how to approach writing a personal note. You are not alone. One of the most common questions that we get is around what to say. Here, we give you a few tips for expanding what you write. We also provide...","categories": ["questions"],
+        "tags": ["gifts"],
+        "url": "http://blog.gifts-done.com/notebirthday/",
+        "teaser":"http://blog.gifts-done.com/assets/images/note.png"},{
         "title": "June Instagram Challenge",
-        "excerpt":"“Life is short - smile while you still have teeth.” Our heart and soul is in our wonderful suppliers, and we want everyone to have a little fun. To that end, we created an instagram challenge for suppliers, but anyone can join the fun. Let’s see what direction your month...","categories": ["suppliers"],
-        "tags": ["social","instagram"],
+        "excerpt":"“Life is short - smile while you still have teeth.” Our heart and soul is in our wonderful suppliers, and we want everyone to have a little fun. To that end, we created an instagram challenge for suppliers, but anyone can join the fun. Let’s see what direction your month...","categories": ["inside"],
+        "tags": ["social","suppliers"],
         "url": "http://blog.gifts-done.com/june-ig/",
-        "teaser":null},{
+        "teaser":"http://blog.gifts-done.com/assets/images/abstract-12.jpg"},{
+        "title": "Healthy Summer Snacks",
+        "excerpt":"Summer food can be the tastiest - barbecues, ice cream, sno cones, fair food. But our tasty treats usually take hours to prepare and are not all that healthy That’s the conundrum with summer. We want to be healthy and have the energy to spend all day outside, and we...","categories": ["life"],
+        "tags": ["food"],
+        "url": "http://blog.gifts-done.com/healthy-summer-snacks/",
+        "teaser":"http://blog.gifts-done.com/assets/images/patrick-fore-389419-unsplash.jpg"},{
         "title": "22 Self Care Ideas",
-        "excerpt":"How are you feeling? Like, right now? If you feel tense, overwhelmed, nervous, or uncomfortable, it may be time to take a self-care break. Because we love you, we spent a few minutes coming up with a list of 22 ideas for self care. You can try these as written...","categories": ["users","suppliers"],
+        "excerpt":"How are you feeling? Like, right now? If you feel tense, overwhelmed, nervous, or uncomfortable, it may be time to take a self-care break. Because we love you, we spent a few minutes coming up with a list of 22 ideas for self care. You can try these as written...","categories": ["life"],
         "tags": ["selfcare"],
         "url": "http://blog.gifts-done.com/22selfcare/",
-        "teaser":null},{
+        "teaser":"http://blog.gifts-done.com/assets/images/abstract-12.jpg"},{
         "title": "July Instagram Challenge",
-        "excerpt":"“Summer is a state of mind” Our heart and soul is in our wonderful suppliers, and we want everyone to have a little fun this summer. To that end, we are continuing our instagram challenge for suppliers, and anyone can join the fun. Let’s see what direction your month takes!...","categories": ["suppliers"],
-        "tags": ["social","instagram"],
+        "excerpt":"“Summer is a state of mind” Our heart and soul is in our wonderful suppliers, and we want everyone to have a little fun this summer. To that end, we are continuing our instagram challenge for suppliers, and anyone can join the fun. Let’s see what direction your month takes!...","categories": ["inside"],
+        "tags": ["social","suppliers"],
         "url": "http://blog.gifts-done.com/july-ig/",
-        "teaser":null},{
+        "teaser":"http://blog.gifts-done.com/assets/images/abstract-12.jpg"},{
         "title": "5 Fun Summer Gifts",
-        "excerpt":"We love summer. Do you love summer? We’ve been chatting up our friends and scrolling through Instagram, of course, to find a few fun summer gifts. Give them to yourself or someone else for whatever occasion inspires you this summer. Our short summer gift idea list You don’t even have...","categories": ["users"],
-        "tags": ["gifts","ideas"],
-        "url": "http://blog.gifts-done.com/fun-summer-gifts/",
-        "teaser":null},{
-        "title": "5 Free Wallpapers from Gifts Done",
-        "excerpt":"Who’s awesome? You’re awesome. Here at Gifts Done, we constantly get to see amazing small business makers coming up with just the right gifts and our users celebrating with their friends and family. We love all of you. We made a few free wallpapers that you can download to decorate...","categories": ["perks"],
+        "excerpt":"We love summer. Do you love summer? We’ve been chatting up our friends and scrolling through Instagram, of course, to find a few fun summer gifts. Give them to yourself or someone else for whatever occasion inspires you this summer. Our short summer gift idea list You don’t even have...","categories": ["tips"],
         "tags": ["gifts"],
+        "url": "http://blog.gifts-done.com/fun-summer-gifts/",
+        "teaser":"http://blog.gifts-done.com/assets/images/abstract-12.jpg"},{
+        "title": "5 Free Wallpapers from Gifts Done",
+        "excerpt":"Who’s awesome? You’re awesome. Here at Gifts Done, we constantly get to see amazing small business makers coming up with just the right gifts and our users celebrating with their friends and family. We love all of you. We made a few free wallpapers that you can download to decorate...","categories": ["inside"],
+        "tags": ["perks"],
         "url": "http://blog.gifts-done.com/5freewallpaper/",
-        "teaser":null}]
+        "teaser":"http://blog.gifts-done.com/assets/images/abstract-12.jpg"},{
+        "title": "5 Fun Back to School Gifts",
+        "excerpt":"Even if you are not in school, the back-to-school vibe gets you. Parents, kids, and teachers are all in the thick of it. They are out buying boring things from lists, things they need, sometimes things that they don’t even want. Our short back to school gift idea list You...","categories": ["tips"],
+        "tags": ["gifts"],
+        "url": "http://blog.gifts-done.com/fun-back-to-school-gifts/",
+        "teaser":"http://blog.gifts-done.com/assets/images/school-gift-ideas.png"},{
+        "title": "No Waste Wrapping",
+        "excerpt":"Wrapping paper can really make the gift sparkle and stand out. There are so many choices in the stores, and you can even custom order your own. Paper for any occasion or preference is out there for us. We (all of people) seem to like it. And why not, it...","categories": ["tips","questions"],
+        "tags": ["gifts"],
+        "url": "http://blog.gifts-done.com/no-waste-wrapping/",
+        "teaser":"http://blog.gifts-done.com/assets/images/abstract-12.jpg"}]
